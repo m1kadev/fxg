115d129cbf5d29325ac0657343510069ebb0df21
m1kadev <67912368+m1kadev@users.noreply.github.com>
2025-11-04 12:07:17 +0100

    replace regex with parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,26 +1,36 @@
 use std::{
+    cmp::min,
     fs::File,
     io::{BufRead, BufReader},
 };
 
-use regex::{Captures, Regex};
-
-struct LineFormatRegexes {
-    code: Regex,
-    bold: Regex,
-    italicised: Regex,
-    underlined: Regex,
+// ! ONLY VALID WITH parse_text IN SCOPE
+macro_rules! markup {
+    ($input:ident, $markup:literal - $markup_end:literal ! $escape:literal -> <$html_tag:ident>) => {{
+        let mut tag_contents = String::new();
+        while let Some(idx) = $input.find($markup_end) {
+            // not escaped; end of input
+            if &$input[idx - 1..idx] != "\\" {
+                tag_contents.push_str(&$input[..idx]);
+                break;
+            } else {
+                // character is escaped, so
+                $input = $input.replacen($escape, "\u{E000}", 1);
+            }
+        }
+        // parse the contents again for tested markup
+        tag_contents = tag_contents.trim().to_string();
+        let parsed = parse_text(&tag_contents);
+        format!(
+            "<{}>{}</{}>",
+            stringify!($html_tag),
+            parsed.replace("\u{E000}", $markup),
+            stringify!($html_tag)
+        )
+    }};
 }
 
-// TODO: replace regexes witha cutal code cause this isnt working
-
 pub fn parse(reader: &mut BufReader<File>) -> String {
-    let regexes = LineFormatRegexes {
-        code: Regex::new(r"<>(?<contents>.*?(\\\\|[^\\]))<\/>").unwrap(),
-        italicised: Regex::new(r"\/\/(?<contents>.+?)\/\/").unwrap(),
-        bold: Regex::new(r"!!(?<contents>.+?)!!").unwrap(),
-        underlined: Regex::new(r"__(?<contents>.+?)__").unwrap(),
-    };
     let mut output = String::new();
     let mut lnbuf = String::new();
 
@@ -33,96 +43,131 @@ pub fn parse(reader: &mut BufReader<File>) -> String {
             break;
         }
         if lnbuf.starts_with('=') {
-            parse_title(line, &mut output, &regexes);
+            output.push_str(&parse_title(line));
             last_line_was_title = true;
         } else if line.is_empty() && !last_line_was_title {
-            output += "\u{E000}br/\u{E001}";
+            output += "\u{E001}br/\u{E002}";
         } else {
-            parse_text(line, &mut output, &regexes);
+            output.push_str(&parse_text(line));
+            output.push(' ');
             last_line_was_title = false;
         }
         lnbuf.clear();
     }
-    as_escaped_html(&output)
+
+    output.replace("\u{E001}", "<").replace("\u{E002}", ">")
 }
 
-fn parse_title(line: &str, output: &mut String, regexes: &LineFormatRegexes) {
+fn parse_title(line: &str) -> String {
+    let mut output = String::new();
     let (prefix, _) = line.split_once(' ').unwrap();
     let header_size = prefix.len();
     // header syntax is complete
 
     if line.ends_with(&str::repeat("=", header_size)) {
-        output.push_str("\u{E000}h");
+        output.push_str("\u{E001}h");
         output.push_str(&header_size.to_string());
-        output.push('\u{E001}');
+        output.push('\u{E002}');
         let header_contents = &line[header_size..line.len() - 1 - header_size].trim();
-        parse_text(header_contents, output, regexes);
-        output.push_str("\u{E000}/h");
+        output.push_str(&parse_text(header_contents));
+        output.push_str("\u{E001}/h");
         output.push_str(&header_size.to_string());
-        output.push('\u{E001}');
+        output.push('\u{E002}');
     } else {
         // parse the text normally
-        parse_text(line, output, regexes);
+        output.push_str(&parse_text(line));
     }
+    return output;
 }
 
-fn parse_text(line: &str, output: &mut String, regexes: &LineFormatRegexes) {
-    let with_codeblocks_parsed = regexes.code.replace_all(line, |caps: &Captures| {
-        format!(
-            "\u{E000}code\u{E001}{}\u{E000}/code\u{E001}",
-            as_escaped_fxg(&caps["contents"])
-        )
-    });
-    let with_bold_parsed = regexes.bold.replace_all(
-        &with_codeblocks_parsed,
-        "\u{E000}strong\u{E001}$contents\u{E000}/strong\u{E001}",
-    );
-    let with_italicised_parsed = regexes.italicised.replace_all(
-        &with_bold_parsed,
-        "\u{E000}em\u{E001}$contents\u{E000}/em\u{E001}",
-    );
-    let with_underline_parsed = regexes.underlined.replace_all(
-        &with_italicised_parsed,
-        "\u{E000}u\u{E001}$contents\u{E000}/u\u{E001}",
-    );
+fn parse_text(line: &str) -> String {
+    let mut output = String::new();
+    // find opening tag
+    let cursive = line.find("//");
+    let bold = line.find("!!");
+    let underline = line.find("__");
+    let code = line.find("<>");
+    let smallest = [cursive, bold, underline, code]
+        .iter()
+        .filter(|x| x.is_some())
+        .map(|x| x.unwrap())
+        .min();
+    if smallest.is_none() {
+        return line.to_string();
+    } else {
+        if smallest == cursive {
+            let idx = smallest.unwrap();
+            output.push_str(&line[..idx]);
+            let mut fmt_text = line[idx + 2..].to_string();
+            let compiled = markup!(fmt_text, "//" - "//" ! "\\//" -> <em>);
+            output.push_str(&compiled);
+            let text_length = compiled.len() - 9;
+            output.push_str(&parse_text(&fmt_text[text_length + 2..]));
+        } else if smallest == bold {
+            let idx = smallest.unwrap();
+            output.push_str(&line[..idx]);
+            let mut fmt_text = line[idx + 2..].to_string();
+            let compiled = markup!(fmt_text, "!!" - "!!" ! "\\!!" -> <strong>);
+            output.push_str(&compiled);
+            let text_length = compiled.len() - 17;
+            output.push_str(&parse_text(&fmt_text[text_length + 2..]));
+        } else if smallest == underline {
+            let idx = smallest.unwrap();
+            output.push_str(&line[..idx]);
+            let mut fmt_text = line[idx + 2..].to_string();
+            let compiled = markup!(fmt_text, "__" - "__" ! "\\__" -> <u>);
+            output.push_str(&compiled);
+            let text_length = compiled.len() - 7;
+            output.push_str(&parse_text(&fmt_text[text_length + 2..]));
+        } else if smallest == code {
+            let idx = smallest.unwrap();
+            output.push_str(&line[..idx]);
+            let mut code_contents = line[idx + 2..].to_string();
+            let mut tag_contents = String::new();
+            let mut leftovers = "";
+            while let Some(idx) = code_contents.find("</>") {
+                let closing_tag = &code_contents[idx - 1..idx + 3];
+                if closing_tag == "\\</>" {
+                    code_contents = code_contents.replacen("\\</>", "\u{E000}", 1);
+                } else {
+                    tag_contents.push_str(&code_contents[..idx]);
+                    leftovers = &code_contents[idx..];
+                    break;
+                }
+            }
+            output.push_str(&format!("<code>{}</code>", tag_contents));
+            output.push_str(&parse_text(leftovers));
+        } else {
+        }
+    }
 
-    output.push_str(&with_underline_parsed);
-    output.push(' ');
-}
+    // if let Some(idx) = line.find("//") {
+    //     // anything before is unformatted
 
-fn as_escaped_fxg(input: &str) -> String {
-    //println!("escaping {input}");
-    input
-        .replace("\\\\\\</>", "\u{E009}")
-        .replace("\\</>", "\u{E008}")
-        .replace('\\', "\u{E002}")
-        .replace('<', "\u{E003}")
-        .replace('>', "\u{E004}")
-        .replace('/', "\u{E005}")
-        .replace('_', "\u{E006}")
-        .replace('!', "\u{E007}")
-}
+    // } else if let Some(idx) = line.find("!!") {
+    //     output.push_str(&line[..idx]);
+    //     let mut fmt_text = line[idx + 2..].to_string();
+    //     output.push_str(&markup!(fmt_text, "!!" - "!!" ! "\\!!" -> <strong>));
+    // } else if let Some(idx) = line.find("__") {
+    //     output.push_str(&line[..idx]);
+    //     let mut fmt_text = line[idx + 2..].to_string();
+    //     output.push_str(&markup!(fmt_text, "__" - "__" ! "\\__" -> <u>));
+    // } else if let Some(idx) = line.find("<>") {
+    //     // code is a special case
+    //     output.push_str(&line[..idx]);
+    //     let mut fmt_text = line[idx + 2..].to_string();
+    //     while let Some(idx) = fmt_text.find("</>") {
+    //         dbg!(&fmt_text[..idx + 3]);
+    //         if &fmt_text[idx - 1..idx] != "\\" {
+    //             break;
+    //         } else {
+    //             fmt_text = fmt_text.replacen("\\</>", "\u{e000}", 1);
+    //         }
+    //     }
 
-fn as_escaped_html(input: &str) -> String {
-    input
-        .replace("&", "&amp;")
-        .replace("<", "&lt;")
-        .replace("\\<", "&lt;")
-        .replace(">", "&gt;")
-        .replace("\\>", "&gt;")
-        .replace("\"", "&qout;")
-        .replace("\\\\", "\\")
-        .replace("\\!", "!")
-        .replace("\\_", "_")
-        .replace("\\/", "/")
-        .replace("\u{E000}", "<")
-        .replace("\u{E001}", ">")
-        .replace("\u{E002}", "\\")
-        .replace("\u{E003}", "&lt;")
-        .replace("\u{E004}", "&gt;")
-        .replace("\u{E005}", "/")
-        .replace("\u{E006}", "_")
-        .replace("\u{E007}", "!")
-        .replace("\u{E008}", "&lt;/&gt;")
-        .replace("\u{E009}", "\\&lt;/&gt;")
+    //     output.push_str(&format!("<code>{}</code>", fmt_text[..idx].to_string()));
+    // } else {
+    //     output.push_str(line);
+    // }
+    output
 }
